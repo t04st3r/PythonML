51046ed6ced1778f22b0440361cd489ff62d74b4
Raffaele Tosti <raffaele@digisin.it>
2018-10-02 16:58:39 +0200

    sklearn logistic function

---
+++ plot_sigmoid.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.axhspan(0.0, 1.0, facecolor='1.0', alpha=1.0, ls='dotted')
+plt.hlines(y=(0.0, 0.5, 1.0), xmin=-7.0, xmax=7.0, linestyles='dotted', colors='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+plt.show()
--- sk_learn.py
+++ sk_learn.py
@@ -1,7 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
+from sklearn.linear_model import Perceptron, LogisticRegression
 from sklearn.metrics import accuracy_score
 from helpers.helper import PlotBoundaries
 import numpy as np
@@ -15,7 +15,7 @@ sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
-ppn = Perceptron(n_iter=1000, eta0=0.1, random_state=0)
+ppn = Perceptron(max_iter=1000, eta0=0.1, random_state=0)
 ppn.fit(X_train_std, y_train)
 
 y_pred = ppn.predict(X_test_std)
@@ -28,9 +28,28 @@ print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
 
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
-bound = PlotBoundaries(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
-plt = bound.plot()
-plt.xlabel('petal length [standardized]')
-plt.ylabel('petal width [standardized]')
-plt.legend(loc='upper left')
-plt.show()
+
+bound_perc = PlotBoundaries(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
+plt_perc = bound_perc.plot()
+plt_perc.xlabel('petal length [standardized]')
+plt_perc.ylabel('petal width [standardized]')
+plt_perc.legend(loc='upper left')
+plt_perc.show()
+
+#Logistic regression classifier
+lr = LogisticRegression(C=1000.0, random_state=0, solver='lbfgs', multi_class='auto')
+lr.fit(X_train_std, y_train)
+bound_lr = PlotBoundaries(X=X_combined_std, y=y_combined, classifier=lr, test_idx=range(105, 150))
+plt_lr = bound_lr.plot()
+plt_lr.xlabel('petal length [standardized]')
+plt_lr.ylabel('petal width [standardized]')
+plt_lr.legend(loc='upper left')
+plt_lr.show()
+
+#format float on numpy array-ndarray
+np.set_printoptions(precision=8, suppress=True)
+
+#predict probability over test samples
+lr_predictions = lr.predict_proba(X_test_std[:1, :])
+print(lr_predictions)
+
